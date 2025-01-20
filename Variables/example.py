5f58b2fe4fc390cbde038902fc5289c5d4d7fcf8
rvpra <rvrocks2@gmail.com>
2025-01-20 10:54:52 +0530

    Adding Variable concepts

---
+++ Variables/example.py
@@ -0,0 +1,14 @@
+# Rules for variable names
+    # Names can not start with a number.
+    # There can be no spaces in the name, use _ instead.
+#     It's considered best practice (PEP8) that names are lowercase.
+
+# Python uses Dynamic Typing
+# This means you can reassign variables to different data types.
+
+a= 10
+print (a)
+print(type(a))
+a= "Python"  #  Reassignment of variables is Ok in python.
+print(a)
+print((type(a)))
